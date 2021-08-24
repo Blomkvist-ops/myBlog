2d1cdcc2a1d70426f803c5c901f41707667cd028
BKBK <lidamao233@163.com>
2021-08-24 01:58:49 -0700

    add delete and edit blog function

--- src/main/java/com/bkbk/blog/web/admin/BlogController.java
+++ src/main/java/com/bkbk/blog/web/admin/BlogController.java
@@ -97,7 +97,7 @@ public class BlogController {
     @GetMapping("/blogs/{id}/delete")
     public String delete(@PathVariable Long id,RedirectAttributes attributes) {
         blogService.deleteBlog(id);
-        attributes.addFlashAttribute("message", "删除成功");
+        attributes.addFlashAttribute("message", "Deleted successfully.");
         return REDIRECT_LIST;
     }
 
